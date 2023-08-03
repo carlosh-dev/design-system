6a0bbfc4316ef03d59a356434769f7810efbd3bb
Carlos <caique.h@hotmail.com>
2023-08-02 21:46:00 -0300

    first commit

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,7 @@
+export type ButtonProps = {
+  variant?: 'primary' | 'secondary'
+}
+
+export function Button() {
+  return <button>Button</button>
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@carlosh-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+  theme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    width: 'space',
+    height: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
+  },
+})
